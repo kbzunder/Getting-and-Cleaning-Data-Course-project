f153ea602d2d972a312e07e1e231ee9c5e80fd21
kbzunderpepwd <kbzunder@gmail.com>
2020-09-23 23:24:07 +0300

    factor names changed

--- run_analysis.R
+++ run_analysis.R
@@ -17,6 +17,9 @@ TrainData_full<-cbind(TrainData,TrainLabels)
 colnames(TestData)<-Features$V2
 colnames(TrainData)<-Features$V2
 Merged<-rbind(TestData_full,TrainData_full)
-dim(Merged)
 ActivityLabels <-import("activity_labels.txt")
-
+ActivityLabels
+Merged$Activity<-as.factor(as.character(Merged$Activity))
+Merged$Activiy
+Merged$Activ<-factor(Merged$Activity, levels=ActivityLabels$V1, labels=ActivityLabels$V2)
+Merged$Activ
