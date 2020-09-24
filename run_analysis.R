9618a55830d36526da897dbeff0becf8868d570c
kbzunderpepwd <kbzunder@gmail.com>
2020-09-24 11:30:16 +0300

    Nearly final

--- run_analysis.R
+++ run_analysis.R
@@ -8,7 +8,6 @@ colnames(TestData)<-Features$V2
 TestLabels<-import("Y_test.txt")
 colnames(TestLabels)<-"Activity"
 TestData_full<-cbind(TestData,TestLabels)
-Features<-import("features.txt")
 TrainData<-import("X_train.txt")
 colnames(TrainData)<-Features$V2
 TrainLabels<-import("y_train.txt")
@@ -18,8 +17,14 @@ colnames(TestData)<-Features$V2
 colnames(TrainData)<-Features$V2
 Merged<-rbind(TestData_full,TrainData_full)
 ActivityLabels <-import("activity_labels.txt")
-ActivityLabels
 Merged$Activity<-as.factor(as.character(Merged$Activity))
-Merged$Activiy
 Merged$Activ<-factor(Merged$Activity, levels=ActivityLabels$V1, labels=ActivityLabels$V2)
-Merged$Activ
+std_mean<-grep("(std|mean)\\(\\)",names(Merged),value = TRUE)
+Selected_Merged<-select(Merged,std_mean)
+Selected_Merged
+ColNams<-colnames(Selected_Merged)
+ColNames_beautified<-gsub("[()]","",ColNams)
+colnames(Selected_Merged)<-ColNames_beautified
+Selected_Merged
+tidy_data<-summarise_all(Selected_Merged,mean)
+
